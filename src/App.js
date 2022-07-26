ebab94eb6b5ac1b531ce4d5bd3d01fcc3e16390c
shwethaj1104 <shwethaj1104@gmail.com>
2022-07-26 17:19:01 +0530

    changes to show list of tours and styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
 import React, { useState, useEffect } from 'react'
 import Loading from './Loading'
 import Tours from './Tours'
+import data from './data'
 
-const url = ''
+const url = 'https://course-api.com/react-tours-project'
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading,setLoading] = useState(false);
+  const [tours,setTours] = useState([]);
+
+  const removeTour = (id) =>{
+    const newTours = tours.filter((tour)=>{
+      return tour.id !== id;
+    })
+    setTours(newTours)
+  }
+
+  const fetchTours = async() =>{
+    try {
+      setLoading(true)
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false)
+      setTours(tours)
+    } catch (error) {
+      setLoading(true)
+      console.log(error)
+    }
+  }
+
+  useEffect(()=>{
+    fetchTours();
+  },[])
+
+  if(tours.length !==0){
+    return (
+      <main>
+        {(loading  && tours.length !== 0) ? <Loading /> : <Tours tours={tours} removeTour={removeTour}/>}
+      </main>
+    )
+  }
+    
+if(tours.length ===0){
+  return(
+    <main>
+      <div className='title'>
+        <h2>No Tours Left</h2>
+        <button className='btn' onClick={fetchTours}>Refresh</button>
+      </div>
+    </main>
+  )
+}
 }
 
 export default App
--- src/Loading.js
+++ src/Loading.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Loading = () => {
   return (
     <div className="loading">
+      <div className='loading_animation'></div>
       <h1>loading...</h1>
     </div>
   );
