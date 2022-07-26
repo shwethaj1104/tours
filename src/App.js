313077cfda9439cd9d4efad46099002202af244e
shwethaj1104 <shwethaj1104@gmail.com>
2022-07-26 16:03:35 +0530

    initial setup

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react'
+import Loading from './Loading'
+import Tours from './Tours'
+
+const url = ''
+function App() {
+  return <h2>Tours Project Setup</h2>
+}
+
+export default App
